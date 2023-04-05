20ca3917cc56afd067be1f76d9c78eacfb656b8a
Eric Defore <eric.defore@gmail.com>
2023-04-04 21:29:37 -0400

    Implement WooThemes Testimonials functionality
    This only triggers if the plugin doesn't exist (which it shouldn't, as
    it is abandonned and is incompatible with PHP 8)

--- functions.php
+++ functions.php
@@ -372,3 +372,360 @@ add_filter( 'use_block_editor_for_post_type', function( $bool, $post_type ) {
     return true;
 
 }, 999, 2 );
+
+if ( ! function_exists( 'woothemes_get_testimonials' ) ) {
+
+	/**
+	 * Reimplements woothemes_get_testimonials() from the WooThemes Testimonials plugin
+	 * 
+	 * @param  string/array $args  Arguments.
+	 * 
+	 * @since  {{VERSION}}
+	 * @return array/boolean       Array if true, boolean if false
+	 */
+	function woothemes_get_testimonials( $args = '' ) {
+
+		$defaults = array(
+			'limit' => 5,
+			'orderby' => 'menu_order',
+			'order' => 'DESC',
+			'id' => 0,
+			'category' => 0
+		);
+
+		$args = wp_parse_args( $args, $defaults );
+
+		// Allow child themes/plugins to filter here.
+		$args = apply_filters( 'woothemes_get_testimonials_args', $args );
+
+		// The Query Arguments.
+		$query_args = array();
+		$query_args['post_type'] = 'testimonial';
+		$query_args['numberposts'] = $args['limit'];
+		$query_args['orderby'] = $args['orderby'];
+		$query_args['order'] = $args['order'];
+		$query_args['suppress_filters'] = false;
+
+		$ids = explode( ',', $args['id'] );
+
+		if ( 0 < intval( $args['id'] ) && 0 < count( $ids ) ) {
+			$ids = array_map( 'intval', $ids );
+			if ( 1 == count( $ids ) && is_numeric( $ids[0] ) && ( 0 < intval( $ids[0] ) ) ) {
+				$query_args['p'] = intval( $args['id'] );
+			} else {
+				$query_args['ignore_sticky_posts'] = 1;
+				$query_args['post__in'] = $ids;
+			}
+		}
+
+		// Whitelist checks.
+		if ( ! in_array( $query_args['orderby'], array( 'none', 'ID', 'author', 'title', 'date', 'modified', 'parent', 'rand', 'comment_count', 'menu_order', 'meta_value', 'meta_value_num' ) ) ) {
+			$query_args['orderby'] = 'date';
+		}
+
+		if ( ! in_array( $query_args['order'], array( 'ASC', 'DESC' ) ) ) {
+			$query_args['order'] = 'DESC';
+		}
+
+		if ( ! in_array( $query_args['post_type'], get_post_types() ) ) {
+			$query_args['post_type'] = 'testimonial';
+		}
+
+		$tax_field_type = '';
+
+		// If the category ID is specified.
+		if ( is_numeric( $args['category'] ) && 0 < intval( $args['category'] ) ) {
+			$tax_field_type = 'id';
+		}
+
+		// If the category slug is specified.
+		if ( ! is_numeric( $args['category'] ) && is_string( $args['category'] ) ) {
+			$tax_field_type = 'slug';
+		}
+
+		// Setup the taxonomy query.
+		if ( '' != $tax_field_type ) {
+			$term = $args['category'];
+			if ( is_string( $term ) ) { $term = esc_html( $term ); } else { $term = intval( $term ); }
+			$query_args['tax_query'] = array( array( 'taxonomy' => 'testimonial-category', 'field' => $tax_field_type, 'terms' => array( $term ) ) );
+		}
+
+		// The Query.
+		$query = get_posts( $query_args );
+
+		// The Display.
+		if ( ! is_wp_error( $query ) && is_array( $query ) && count( $query ) > 0 ) {
+			foreach ( $query as $k => $v ) {
+				$meta = get_post_custom( $v->ID );
+
+				// Get the image.
+				$query[$k]->image = pds_get_woothemes_testimonial_image( $v->ID, $args['size'] );
+
+				foreach ( (array)pds_get_woothemes_get_custom_fields_settings() as $i => $j ) {
+					if ( isset( $meta['_' . $i] ) && ( '' != $meta['_' . $i][0] ) ) {
+						$query[$k]->$i = $meta['_' . $i][0];
+					} else {
+						$query[$k]->$i = $j['default'];
+					}
+				}
+			}
+		} else {
+			$query = false;
+		}
+
+		return $query;
+
+	}
+
+}
+
+if ( ! has_action( 'woothemes_testimonials', 'woothemes_testimonials' ) ) {
+	add_action( 'woothemes_testimonials', 'woothemes_testimonials' );
+}
+
+if ( ! function_exists( 'woothemes_testimonials' ) ) {
+
+	/**
+	 * Reimplements woothemes_testimonials() from the WooThemes Testimonials plugin
+	 * 
+	 * @param  string/array $args  Arguments.
+	 * @since  {{VERSION}}
+	 * @return void
+	 */
+	function woothemes_testimonials( $args = '' ) {
+
+		global $post, $more;
+	
+		$defaults = apply_filters( 'woothemes_testimonials_default_args', array(
+			'limit' 			=> 5,
+			'per_row' 			=> null,
+			'orderby' 			=> 'menu_order',
+			'order' 			=> 'DESC',
+			'id' 				=> 0,
+			'display_author' 	=> true,
+			'display_avatar' 	=> true,
+			'display_url' 		=> true,
+			'effect' 			=> 'fade', // Options: 'fade', 'none'
+			'pagination' 		=> false,
+			'echo' 				=> true,
+			'size' 				=> 50,
+			'title' 			=> '',
+			'before' 			=> '<div class="widget widget_woothemes_testimonials">',
+			'after' 			=> '</div>',
+			'before_title' 		=> '<h2>',
+			'after_title' 		=> '</h2>',
+			'category' 			=> 0,
+		) );
+	
+		$args = wp_parse_args( $args, $defaults );
+	
+		// Allow child themes/plugins to filter here.
+		$args = apply_filters( 'woothemes_testimonials_args', $args );
+		$html = '';
+	
+		do_action( 'woothemes_testimonials_before', $args );
+	
+			// The Query.
+			$query = woothemes_get_testimonials( $args );
+	
+			// The Display.
+			if ( ! is_wp_error( $query ) && is_array( $query ) && count( $query ) > 0 ) {
+	
+				$class = '';
+	
+				if ( is_numeric( $args['per_row'] ) ) {
+					$class .= ' columns-' . intval( $args['per_row'] );
+				}
+	
+				if ( 'none' != $args['effect'] ) {
+					$class .= ' effect-' . $args['effect'];
+				}
+	
+				$html .= $args['before'] . "\n";
+				if ( '' != $args['title'] ) {
+					$html .= $args['before_title'] . esc_html( $args['title'] ) . $args['after_title'] . "\n";
+				}
+				$html .= '<div class="testimonials component' . esc_attr( $class ) . '">' . "\n";
+	
+				$html .= '<div class="testimonials-list">' . "\n";
+	
+				// Begin templating logic.
+				$tpl = '<div id="quote-%%ID%%" class="%%CLASS%%" itemprop="review" itemscope itemtype="http://schema.org/Review"><blockquote class="testimonials-text" itemprop="reviewBody">%%TEXT%%</blockquote>%%AVATAR%% %%AUTHOR%%</div>';
+				$tpl = apply_filters( 'woothemes_testimonials_item_template', $tpl, $args );
+	
+				$count = 0;
+				foreach ( $query as $post ) { $count++;
+					$template = $tpl;
+	
+					$css_class = 'quote';
+					if ( ( is_numeric( $args['per_row'] ) && ( $args['per_row'] > 0 ) && ( 0 == ( $count - 1 ) % $args['per_row'] ) ) || 1 == $count ) { $css_class .= ' first'; }
+					if ( ( is_numeric( $args['per_row'] ) && ( $args['per_row'] > 0 ) && ( 0 == $count % $args['per_row'] ) ) || count( $query ) == $count ) { $css_class .= ' last'; }
+	
+					// Add a CSS class if no image is available.
+					if ( isset( $post->image ) && ( '' == $post->image ) ) {
+						$css_class .= ' no-image';
+					}
+	
+					setup_postdata( $post );
+	
+					$author = '';
+					$author_text = '';
+	
+					// If we need to display the author, get the data.
+					if ( ( get_the_title( $post ) != '' ) && true == $args['display_author'] ) {
+						$author .= '<cite class="author" itemprop="author" itemscope itemtype="http://schema.org/Person">';
+	
+						$author_name = '<span itemprop="name">' . get_the_title( $post ) . '</span>';
+	
+						$author .= $author_name;
+	
+						if ( isset( $post->byline ) && '' != $post->byline ) {
+							$author .= ' <span class="title" itemprop="jobTitle">' . $post->byline . '</span><!--/.title-->' . "\n";
+						}
+	
+						if ( true == $args['display_url'] && '' != $post->url ) {
+							$author .= ' <span class="url"><a href="' . esc_url( $post->url ) . '" itemprop="url">' . apply_filters( 'woothemes_testimonials_author_link_text', $text = esc_url( $post->url ) ) . '</a></span><!--/.excerpt-->' . "\n";
+						}
+	
+						$author .= '</cite><!--/.author-->' . "\n";
+	
+						// Templating engine replacement.
+						$template = str_replace( '%%AUTHOR%%', $author, $template );
+					} else {
+						$template = str_replace( '%%AUTHOR%%', '', $template );
+					}
+	
+					// Templating logic replacement.
+					$template = str_replace( '%%ID%%', get_the_ID(), $template );
+					$template = str_replace( '%%CLASS%%', esc_attr( $css_class ), $template );
+	
+					if ( isset( $post->image ) && ( '' != $post->image ) && true == $args['display_avatar'] && ( '' != $post->url ) ) {
+						$template = str_replace( '%%AVATAR%%', '<a href="' . esc_url( $post->url ) . '" class="avatar-link">' . $post->image . '</a>', $template );
+					} elseif ( isset( $post->image ) && ( '' != $post->image ) && true == $args['display_avatar'] ) {
+						$template = str_replace( '%%AVATAR%%', $post->image, $template );
+					} else {
+						$template = str_replace( '%%AVATAR%%', '', $template );
+					}
+	
+					// Remove any remaining %%AVATAR%% template tags.
+					$template 	= str_replace( '%%AVATAR%%', '', $template );
+					$real_more 	= $more;
+					$more      	= 0;
+					$content 	= apply_filters( 'woothemes_testimonials_content', apply_filters( 'the_content', get_the_content( __( 'Read full testimonial...', 'our-team-by-woothemes' ) ) ), $post );
+					$more      	= $real_more;
+					$template 	= str_replace( '%%TEXT%%', $content, $template );
+	
+					// Assign for output.
+					$html .= $template;
+	
+					if( is_numeric( $args['per_row'] ) && ( $args['per_row'] > 0 ) && ( 0 == $count % $args['per_row'] ) ) {
+						$html .= '<div class="fix"></div>' . "\n";
+					}
+				}
+	
+				wp_reset_postdata();
+	
+				$html .= '</div><!--/.testimonials-list-->' . "\n";
+	
+				if ( $args['pagination'] == true && count( $query ) > 1 && $args['effect'] != 'none' ) {
+					$html .= '<div class="pagination">' . "\n";
+					$html .= '<a href="#" class="btn-prev">' . apply_filters( 'woothemes_testimonials_prev_btn', '&larr; ' . __( 'Previous', 'woothemes-testimonials' ) ) . '</a>' . "\n";
+					$html .= '<a href="#" class="btn-next">' . apply_filters( 'woothemes_testimonials_next_btn', __( 'Next', 'woothemes-testimonials' ) . ' &rarr;' ) . '</a>' . "\n";
+					$html .= '</div><!--/.pagination-->' . "\n";
+				}
+					$html .= '<div class="fix"></div>' . "\n";
+				$html .= '</div><!--/.testimonials-->' . "\n";
+				$html .= $args['after'] . "\n";
+			}
+	
+			// Allow child themes/plugins to filter here.
+			$html = apply_filters( 'woothemes_testimonials_html', $html, $query, $args );
+	
+			if ( $args['echo'] != true ) { return $html; }
+	
+			// Should only run is "echo" is set to true.
+			echo $html;
+	
+			do_action( 'woothemes_testimonials_after', $args ); // Only if "echo" is set to true.
+
+	}
+
+}
+
+/**
+ * Reimplements Woothemes_Testimonials::get_image()
+ * 
+ * @param  int 				$id   Post ID.
+ * @param  string/array/int $size Image dimension.
+ * 
+ * @since  {{VERSION}}
+ * @return string       	<img> tag
+ */
+function pds_get_woothemes_testimonial_image( $id, $size ) {
+
+	$response = '';
+
+	if ( has_post_thumbnail( $id ) ) {
+		// If not a string or an array, and not an integer, default to 150x9999.
+		if ( ( is_int( $size ) || ( 0 < intval( $size ) ) ) && ! is_array( $size ) ) {
+			$size = array( intval( $size ), intval( $size ) );
+		} elseif ( ! is_string( $size ) && ! is_array( $size ) ) {
+			$size = array( 50, 50 );
+		}
+		$response = get_the_post_thumbnail( intval( $id ), $size, array( 'class' => 'avatar' ) );
+	} else {
+		$gravatar_email = get_post_meta( $id, '_gravatar_email', true );
+		if ( '' != $gravatar_email && is_email( $gravatar_email ) ) {
+			$response = get_avatar( $gravatar_email, $size );
+		}
+	}
+
+	return $response;
+
+}
+
+/**
+ * Reimplements Woothemes_Testimonials::get_custom_fields_settings()
+ * 
+ * @since  {{VERSION}}
+ * @return array
+ */
+function pds_get_woothemes_get_custom_fields_settings() {
+
+	$fields = array();
+
+	$fields['gravatar_email'] = array(
+		'name' => __( 'Gravatar E-mail Address', 'woothemes-testimonials' ),
+		'description' => sprintf( __( 'Enter in an e-mail address, to use a %sGravatar%s, instead of using the "Featured Image".', 'woothemes-testimonials' ), '<a href="' . esc_url( 'http://gravatar.com/' ) . '" target="_blank">', '</a>' ),
+		'type' => 'text',
+		'default' => '',
+		'section' => 'info'
+	);
+
+	$fields['byline'] = array(
+		'name' => __( 'Byline', 'woothemes-testimonials' ),
+		'description' => __( 'Enter a byline for the customer giving this testimonial (for example: "CEO of WooThemes").', 'woothemes-testimonials' ),
+		'type' => 'text',
+		'default' => '',
+		'section' => 'info'
+	);
+
+	$fields['url'] = array(
+		'name' => __( 'URL', 'woothemes-testimonials' ),
+		'description' => __( 'Enter a URL that applies to this customer (for example: http://woothemes.com/).', 'woothemes-testimonials' ),
+		'type' => 'url',
+		'default' => '',
+		'section' => 'info'
+	);
+
+	return $fields;
+
+}
+
+// Prevents the Parent Theme from being upset that this class doesn't exist
+if ( ! class_exists( 'Woothemes_Testimonials ' ) ) {
+	class Woothemes_Testimonials {
+
+	}
+
+}
